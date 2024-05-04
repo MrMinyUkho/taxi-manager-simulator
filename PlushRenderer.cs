8cb3fa8d3524c5e07f9225207f15a0e5bb33588a
Dmytro <ADimaso007@gmail.com>
2024-05-04 19:36:01 +0300

    Прокоментировал плюшевый рендерер

--- PlushRenderer.cs
+++ PlushRenderer.cs
@@ -102,13 +102,20 @@ class Color(int a, int r, int g, int b)
     public static Color operator -(Color a) => new(a.a, -a.r, -a.g, -a.b);
 }
 
+/// <summary>
+/// Использует возможности SDL для упрощённой отрисовки примитивов 
+/// </summary>
 class PlushRenderer
 {
 
     readonly nint renderer;
     readonly Dictionary<string, nint> Fonts;
 
+    /// <summary>
+    /// Создание пустышки для удовлетворения not-null переменной
+    /// </summary>
     public static PlushRenderer Empty { get => new(); }
+
     public PlushRenderer(ref nint renderer)
     {
         Fonts = [];
@@ -123,6 +130,12 @@ class PlushRenderer
 
     #region Features
 
+    /// <summary>
+    /// Отрисока кнопки
+    /// </summary>
+    /// <remarks> Не забудьте загрузить шрифт в рендерер с именем "Button"</remarks>
+    /// <param name="button">Кнопка</param>
+    /// <exception cref="ArgumentException">В слуаче отсутсвия шрифта</exception>
     public void DrawButton(PlushButton button)
     {
         SDL_Rect rect = new()
@@ -140,13 +153,15 @@ class PlushRenderer
             b = button.col.b,
         };*/
 
+        // Рисуем прямоугольник кнопки
         DrawRectF(rect.x,
                   rect.y,
                   rect.w,
                   rect.h,
                   button.col);
 
-        if (!Fonts.TryGetValue("Button", out nint value)) throw new ArgumentException($"Button font not found");
+        // Тут делаем всё так же как и в отрисовке текста, 
+        if (!Fonts.TryGetValue("Button", out nint value)) throw new ArgumentException($"Button fontName not found");
 
         nint surfaceMessage = TTF_RenderText_Solid(value, button.text, new() { a = 255, r = 255, b = 255, g = 255 });
 
@@ -155,6 +170,7 @@ class PlushRenderer
         SDL_Rect Message_rect = new();
         _ = SDL_QueryTexture(Message, out _, out _, out Message_rect.w, out Message_rect.h);
 
+        // но координаты считаем как отступ от центра кнопки 
         Message_rect.x = rect.x + rect.w / 2 - Message_rect.w / 2;
         Message_rect.y = rect.y + rect.h / 2 - Message_rect.h / 2;
 
@@ -168,6 +184,13 @@ class PlushRenderer
 
     #region Lines
 
+    /// <summary>
+    /// Нахождение нормали к вектору по двум точкам
+    /// </summary>
+    /// <param name="a">Первая точка</param>
+    /// <param name="b">Вторая точка</param>
+    /// <param name="invert">Менять ли направление</param>
+    /// <returns></returns>
     private static float[] GetLineNormal(float[] a,
                                          float[] b,
                                          bool invert)
@@ -191,13 +214,28 @@ class PlushRenderer
         return c;
     }
 
+    /// <summary>
+    /// Отрисовывает линии из массива пар чисел определённой толщины
+    /// </summary>
+    /// <remarks>
+    /// Толщина создаётся с помощью тупейшего костыля в виде 
+    /// множества линий с отступом в полпикселя в стороны
+    /// </remarks>
+    /// <param name="points">Точки</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="thinkness">Толщина</param>
+    /// <param name="a">Для рекурсивного вызова, без разницы чё туда передать</param>
+    /// <exception cref="ArgumentException">С массивом точек явно что-то не то</exception>
     public void DrawLines(List<float[]> points, Color col, float thinkness, bool a = true)
     {
+        // Смотрим попал ли какой-то мусор в список
         for (int i = 0; i < points.Count; ++i)
             if (points[i].Length != 2) throw new ArgumentException("Line simplePoints must have only two coordinates");
 
+        // Базовое условие
         if (thinkness < 0) return;
 
+        // Массив точек
         SDL_FPoint[] SDL_points = new SDL_FPoint[(points.Count * 2) - 2];
 
         for (int i = 1; i < points.Count; ++i)
@@ -207,6 +245,7 @@ class PlushRenderer
 
             float[] dir = GetLineNormal(points[i - 1], points[i], a);
 
+            // Линнии с отступами в стороны для создания толщины
             pointS.x = points[i - 1][0] + (dir[0] * (thinkness - 1) / 2);
             pointS.y = points[i - 1][1] + (dir[1] * (thinkness - 1) / 2);
 
@@ -217,12 +256,20 @@ class PlushRenderer
             SDL_points[(i * 2) - 1] = pointE;
         }
 
+        // Рендерим всё это месиво
         _ = SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
         _ = SDL_RenderDrawLinesF(renderer, SDL_points, SDL_points.Length);
 
-        DrawLines(points, col, thinkness - (float)0.5, !a);
+        DrawLines(points, col, thinkness - (float)0.5, !a); // Рекурсивный вызов
     }
 
+    /// <summary>
+    /// Отрисовка только одной линии
+    /// </summary>
+    /// <param name="a">Начало</param>
+    /// <param name="b">Конец</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="th">Толщина</param>
     public void DrawLine(float[] a, float[] b, Color col, int th)
     {
         DrawLines([a, b], col, th);
@@ -232,44 +279,77 @@ class PlushRenderer
 
     #region NGons
 
-    private void DrawNGon(float x, float y, float r, Color col, int nv, bool mode = false, float defRot = (float)(-Math.PI / 2))
+    private void DrawNGon(float x, float y, float r, Color col, int numVerticies, bool mode = false, float defRot = (float)(-Math.PI / 2))
     {
         if (r <= 0) return;
+        // базовый случай рекурсии, ну или для особо тупых фармошлёпов, которые решили что радиус может меньше нуля
 
-        float dt = (float)(Math.PI * 2) / nv;
-        SDL_FPoint[] points = new SDL_FPoint[nv + 1];
+        float dt = (float)(Math.PI * 2) / numVerticies;            // угловой размер линии
+        SDL_FPoint[] points = new SDL_FPoint[numVerticies + 1];
 
-        for (int i = 0; i <= nv; ++i)
+        for (int i = 0; i <= numVerticies; ++i)
         {
             points[i] = new SDL_FPoint
             {
-                x = x + (r * (float)Math.Cos((dt * i) + defRot)),
+                x = x + (r * (float)Math.Cos((dt * i) + defRot)),  // вращаем точки вокруг центра на шаг
                 y = y + (r * (float)Math.Sin((dt * i) + defRot))
             };
         }
 
-        _ = SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
-        _ = SDL_RenderDrawLinesF(renderer, points, nv + 1);
+        _ = SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);  // рисуем
+        _ = SDL_RenderDrawLinesF(renderer, points, numVerticies + 1);
 
-        if (mode) DrawNGon(x, y, r - (float)0.5, col, nv, true, defRot);
+        if (mode) DrawNGon(x, y, r - (float)0.5, col, numVerticies, true, defRot); 
+        // В случае заливки делаем рекрсивный вызов с теми же аргументами, только радиус немного уменьшаем
     }
 
-    public void DrawNGonF(float x, float y, float r, Color col, int nv, float defRot = (float)(-Math.PI / 2))
+    /// <summary>
+    /// Отрисовка правильного многоугольника с заливкой
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координата по Y</param>
+    /// <param name="r">Радиус</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="numVerticies">Количество вершин</param>
+    /// <param name="defRot">Поворот вокруг центра в радианах</param>
+    public void DrawNGonF(float x, float y, float r, Color col, int numVerticies, float defRot = (float)(-Math.PI / 2))
     {
-        DrawNGon(x, y, r, col, nv, true, defRot);
+        DrawNGon(x, y, r, col, numVerticies, true, defRot);
     }
 
-    public void DrawNGonW(float x, float y, float r, Color col, int nv, float defRot = (float)(-Math.PI / 2))
+    /// <summary>
+    /// Отрисовка контура правильного многоугольника
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координата по Y</param>
+    /// <param name="r">Радиус</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="numVerticies">Количество вершин</param>
+    /// <param name="defRot">Поворот вокруг центра в радианах</param>
+    public void DrawNGonW(float x, float y, float r, Color col, int numVerticies, float defRot = (float)(-Math.PI / 2))
     {
-        DrawNGon(x, y, r, col, nv, false, defRot);
+        DrawNGon(x, y, r, col, numVerticies, false, defRot);
     }
 
+    /// <summary>
+    /// Отрисовка круга
+    /// </summary>
+    /// <param name="x">Координаты по X</param>
+    /// <param name="y">Координаты по Y</param>
+    /// <param name="r">Радиус</param>
+    /// <param name="col">Цвет</param>
     public void DrawCircleF(float x, float y, float r, Color col)
     {
         DrawNGon(x, y, r, col, 100, true);
-        //filledCircleRGBA(renderer, (short)x, (short)y, (short)r, col.r, col.g, col.b, col.a);
     }
 
+    /// <summary>
+    /// Отрисовка окружности
+    /// </summary>
+    /// <param name="x">Координаты по X</param>
+    /// <param name="y">Координаты по Y</param>
+    /// <param name="r">Радиус</param>
+    /// <param name="col">Цвет</param>
     public void DrawCircleW(float x, float y, float r, Color col)
     {
         DrawNGon(x, y, r, col, 150);
@@ -279,22 +359,42 @@ class PlushRenderer
 
     #region PlushText
 
+    /// <summary>
+    /// Инициализация SDL TTF
+    /// </summary>
     public static void InitText()
     {
         _ = TTF_Init();
     }
 
+    /// <summary>
+    /// Добавление шрифта в "библиотеку" шрифтов отдельно взятого рендерера
+    /// </summary>
+    /// <remarks>
+    /// Если не указано имя, то берёться название файла без расширения
+    /// </remarks>
+    /// <param name="path">Где лежит шрифт</param>
+    /// <param name="size">Размер</param>
+    /// <param name="name">Имя шрифта которое будет использовано дальше в коде</param>
     public void LoadFont(string path, int size, string? name = null)
     {
-
         nint new_font = TTF_OpenFont(path, size);
-        string new_name = path.Split('.')[0];
+        string new_name = path.Split('.').First().Split("/").Last(); // Берём либо имя, либо название файла
         if (name != null) new_name = name;
 
         Fonts.Add(new_name, new_font);
     }
 
-    public void DrawText(int x, int y, string s, Color col, string font)
+    /// <summary>
+    /// Рисует шрифт
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координаты по Y</param>
+    /// <param name="s">Текст</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="fontName">Имя шрифта указаное в <c>LoadFont</c></param>
+    /// <exception cref="ArgumentException">Если шрифт не найден</exception>
+    public void DrawText(int x, int y, string s, Color col, string fontName)
     {
         SDL_Color White = new()
         {
@@ -304,18 +404,24 @@ class PlushRenderer
             a = col.a
         };
 
-        if (!Fonts.TryGetValue(font, out nint value)) throw new ArgumentException($"Font {font} not found");
+        // Проверяем наличие шрифта
+        if (!Fonts.TryGetValue(fontName, out nint font)) throw new ArgumentException($"Font {fontName} not found");
 
-        nint surfaceMessage = TTF_RenderText_Solid(value, s, White);
+        // Рендерим текст
+        nint surfaceMessage = TTF_RenderText_Solid(font, s, White);
 
+        // Фигачим всё в текстуру
         nint Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 
         SDL_Rect Message_rect = new();
+        // Получаем размеры текстуры
         _ = SDL_QueryTexture(Message, out _, out _, out Message_rect.w, out Message_rect.h);
 
+        // Координаты
         Message_rect.x = x;
         Message_rect.y = y;
 
+        // Рендерим текстуру в кадре
         _ = SDL_RenderCopy(renderer, Message, (nint)null, ref Message_rect);
 
         SDL_FreeSurface(surfaceMessage);
@@ -326,6 +432,15 @@ class PlushRenderer
 
     #region Rects
 
+    /// <summary>
+    /// Отрисовывает прямоугольник(ого какая неожиданность)
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координата по Y</param>
+    /// <param name="w">Ширина</param>
+    /// <param name="h">Высота</param>
+    /// <param name="col">Цвет</param>
+    /// <param name="mode">С заливкой или нет</param>
     private void DrawRect(float x, float y, float w, float h, Color col, bool mode)
     {
         _ = SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a);
@@ -342,11 +457,27 @@ class PlushRenderer
             _ = SDL_RenderDrawRect(renderer, ref rect);
     }
 
+    /// <summary>
+    /// Отрисовывает прямоугольник с заливкой
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координата по Y</param>
+    /// <param name="w">Ширина</param>
+    /// <param name="h">Высота</param>
+    /// <param name="col">Цвет</param>
     public void DrawRectF(float x, float y, float w, float h, Color col)
     {
         DrawRect(x, y, w, h, col, true);
     }
 
+    /// <summary>
+    /// Отрисовывает контур прямоугольника
+    /// </summary>
+    /// <param name="x">Координата по X</param>
+    /// <param name="y">Координата по Y</param>
+    /// <param name="w">Ширина</param>
+    /// <param name="h">Высота</param>
+    /// <param name="col">Цвет</param>
     public void DrawRectW(float x, float y, float w, float h, Color col)
     {
         DrawRect(x, y, w, h, col, false);
@@ -356,6 +487,10 @@ class PlushRenderer
 
     #region SDL_tools
 
+    /// <summary>
+    /// Заливает окно сплошным цветом
+    /// </summary>
+    /// <param name="col">Цвет</param>
     public void FillWindow(Color col)
     {
         if (SDL_SetRenderDrawColor(renderer, col.r, col.g, col.b, col.a) < 0)
@@ -365,6 +500,9 @@ class PlushRenderer
             Console.WriteLine($"There was an issue with clearing the render surface. {SDL_GetError()}");
     }
 
+    /// <summary>
+    /// Отрисовывает новый кадр на экране. Вызывать надо после других вызовов отрисовки
+    /// </summary>
     public void Redraw()
     {
         // Switches out the currently presented render surface with the one we just did work on.
